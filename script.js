88e80fdf6ca02b087767ae1d33a19624402aac77
Mirinda <iryna.dmytrash@gmail.com>
2025-01-01 01:40:09 +0100

    sedrftgyhu

---
+++ script.js
@@ -0,0 +1,69 @@
+// document.getElementById("count-el").innerText = 5;
+
+
+
+// let myAge = 32;
+
+// console.log(myAge);
+
+// let myAge = 10;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge);
+
+
+// let count = 0;
+
+// count = count + 1;
+
+// console.log(count);
+
+
+
+// let bonusPoints = 50;
+
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// function increment() {
+// console.log("The button was clicked");
+
+// }
+
+// function countdown() {
+//     console.log(5);
+//     console.log(4);
+//     console.log(3);
+//     console.log(2);
+//     console.log(1);
+// }
+
+// countdown();
+
+// countdown();
+
+// function myLogger() {
+//     console.log(5);
+//     console.log(4);
+//     console.log(3);
+//     console.log(2);
+//     console.log(1);
+// }
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function lapTime() {
+    let totalTime= lap1 + lap2 + lap3;
+    console.log(totalTime);
+}
+
+lapTime();
