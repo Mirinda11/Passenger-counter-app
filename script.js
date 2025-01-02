ad368a08b13f89a8f6772651eecff53adaf51f76
Mirinda <iryna.dmytrash@gmail.com>
2025-01-02 07:32:21 +0100

    02.01.last

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// document.getElementById("count-el").innerText = 5;
+
 
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
@@ -17,25 +17,3 @@ function save() {
     countEl.textContent = 0;
     count = 0;
 }
-
-// function result(){
-//     let message = " Previous entires: " + count;
-//     saveEl.innerText = message;
-//     console.log(message);
-// }
-
-
-// let saveBtn = document.getElementById("save-btn");
-
-
-// let message = " people in the train";
-
-
-// // console,log(saveBtn);
-
-// function save() {
-//     saveBtn.innerText = count + message;
-//     console.log(message);
-
-// }
-
